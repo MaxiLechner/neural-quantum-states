2eef72dbff1b3e2a094261d659fd4d5b6f1db060
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-11-04 16:48:13 +0100

    complete rework of wavefunction. now uses complex variables

--- wavefunction.py
+++ wavefunction.py
@@ -1,40 +1,55 @@
-from jax import random, grad
 import jax.numpy as np
+from jax.lib import pytree
 
 
-def psi(predictions):
-    """computes the wavefunction psi from the unormalized input vectors
-    given by the output of the autoregressive model"""
-    lnpsi, lnpsi_i = logpsi(predictions)
-    psi_i = np.exp(lnpsi_i)
-    psi = np.exp(lnpsi)
-    p2 = np.prod(psi_i, axis=1)
-    return psi, p2, psi_i
+def real_to_complex(arr):
+    _, _, s3 = arr.shape
+    assert s3 == 4
+    carr = arr[:, :, [0, 2]] * np.exp(arr[:, :, [1, 3]] * 1j)
+    return carr
 
 
-def logpsi(predictions):
-    """computes the log-wavefunction from the unormalized input vectors
-    given by the output of the autoregressive model"""
-    l2norm = np.exp(predictions)
-    l2norm = np.absolute(l2norm) ** 2
-    l2norm = np.sum(l2norm, axis=2)
-    l2norm = np.expand_dims(l2norm, axis=2)
-    lnpsi_i = predictions - 0.5 * np.log(l2norm)
-    lnpsi = np.sum(lnpsi_i, axis=1)
-    return lnpsi, lnpsi_i
+def compute_probs(arr):
+    arr = real_to_complex(arr)
+    tc = np.exp(arr)
+    nc = np.linalg.norm(tc, 2, axis=2, keepdims=True)
+    tc = tc / nc
+    probs = np.square(np.abs(tc))
+    return probs
 
 
-def sample(state, key, i, N):
-    """draws samples from the wavefunction psi"""
-    wf = psi(state)
-    sqnorm = (np.linalg.norm(psi, axis=2) ** 2)[0][i]
-    rand = random.bernoulli(key, sqnorm) * 2 - 1
-    # print(sqnorm,rand)
-    for i in range(N):
-        wavefunction = psi(state)
-        # print(vis)
-        key, subkey = random.split(key)
-        ss = sample(vis, key, i)
-        print(ss)
-        data[0][i] = rand
-    return data
+def lpsi(net, data):
+    """compute logpsi for a batch of samples. As the network returns
+    the amplitude for both up and down states we need to pick the
+    right amplitude by indexing according to the samples"""
+    arr = net(data)
+    arr = real_to_complex(arr)
+    tc = np.exp(arr)
+    nc = np.linalg.norm(tc, 2, axis=2, keepdims=True) ** 2
+
+    idx = (data + 1) / 2
+    idx = idx.astype(int)
+    B, N, _ = data.shape
+    splits = np.split(arr, B)
+    splits = [i.reshape(N, 2) for i in splits]
+    isplits = np.split(idx, B)
+    isplits = [i.reshape(N) for i in isplits]
+    vi = np.stack([splits[j][np.arange(N), isplits[j]] for j in range(B)]).reshape(
+        B, N, 1
+    )
+
+    logpsi = vi - 0.5 * np.log(nc)
+    logpsi = np.sum(logpsi, axis=1)
+    return np.real(logpsi), np.imag(logpsi)
+
+
+def make_complex(state):
+    """turns the real valued state into complex form, function modeled after tree_util functions like tree_map"""
+    a, b = state
+    assert len(a) == len(b)
+    leaves, treedef = pytree.flatten(a)
+    leaves2, _ = pytree.flatten(b)
+    out = []
+    for i in range(len(leaves)):
+        out.append(leaves[i] + leaves2[i] * 1j)
+    return treedef.unflatten(out)
