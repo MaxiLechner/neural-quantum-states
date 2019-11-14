bd326953f8046606d98f3f05beb6591bf943e0ea
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-11-14 12:12:17 +0100

    moved itest to example_ising. removed isingtest

--- example_Ising1d.py
+++ example_Ising1d.py
@@ -1,16 +1,55 @@
-from ising1d import initialize_ising1d, ising1d
+from isingtest import initialize_ising1d, step, callback
 from network import net
 
-# from wavefunction import lpsi
-from optim import train
+from jax.experimental import optimizers
 
+from time import time
+import matplotlib.pyplot as plt
+import pdb
 
 if __name__ == "__main__":
+    batchSize = 100
     numSpins = 10
-    batchSize = 100  # batchsize
-    numIt = 1000
-    lr = 1e-02
-    net_apply, net_params, key, lpsi = initialize_ising1d(numSpins, batchSize, net)
-    sample, energy, grad = ising1d(net_apply, net_params, key, N, B, lpsi)
+    net_apply, net_params, key, data = initialize_ising1d(batchSize, numSpins, net)
 
-    train(optimizer, lr, numIt, key, net_params, plotting=True)
+    opt_init, opt_update, get_params = optimizers.adam(1e-02)
+
+    E = []
+    mag = []
+    ratio = []
+
+    fig, ax = plt.subplots()
+    plt.ion()
+    plt.show(block=False)
+
+    opt_state = opt_init(net_params)
+    for i in range(1000):
+        start_time = time()
+        opt_state, energy, magnetization = step(
+            i, key, net_apply, opt_update, get_params, opt_state, data
+        )
+        E.append(energy)
+        mag.append(magnetization)
+        end_time = time()
+        if i % 25 == 0:
+            callback((E, mag, end_time, start_time), i, ax)
+
+    batchSize = 250
+    numSpins = 10
+    net_apply, net_params, key, data = initialize_ising1d(batchSize, numSpins, net)
+
+    for i in range(1000):
+        start_time = time()
+        opt_state, energy, magnetization = step(
+            i, key, net_apply, opt_update, get_params, opt_state, data
+        )
+        E.append(energy)
+        mag.append(magnetization)
+        end_time = time()
+        if i % 25 == 0:
+            callback((E, mag, end_time, start_time), i, ax)
+
+    # pdb.set_trace()
+
+    net_params = get_params(opt_state)
+    plt.show(block=True)
