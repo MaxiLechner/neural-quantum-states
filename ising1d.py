57e18de825ba89bdf430482ff8c610e0155ac6ac
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-11-19 00:27:14 +0100

    refactored calc of energy to make use of a fori_loop to speed up jit compilation. Furthermore added optimizers to initialize_ising function

--- ising1d.py
+++ ising1d.py
@@ -7,16 +7,17 @@ from jax import random
 import jax.numpy as np
 from jax import jit
 from jax.lax import fori_loop
+from jax.experimental import optimizers
 
 from wavefunction import lpsi
-from sampler import sample, sample_fori
+from sampler import sample
 from util import make_complex, apply_elementwise
 
 import matplotlib.pyplot as plt
 from functools import partial
 
 
-def initialize_ising1d(batchSize, numSpins, network):
+def initialize_ising1d(batchSize, numSpins, lr, network):
     M = (
         2 * 2
     )  # number of possible values each input can take times 2 as lax.conv only works with real valued weights
@@ -28,8 +29,9 @@ def initialize_ising1d(batchSize, numSpins, network):
     in_shape = (-1, numSpins, 1)
     _, net_params = net_init(subkey, in_shape)
     net_apply = jit(net_apply)
+    opt_init, opt_update, get_params = optimizers.adam(lr)
     data = np.zeros((batchSize, numSpins, 1), dtype=np.float32)
-    return net_apply, net_params, data, key
+    return net_apply, net_params, data, key, opt_init, opt_update, get_params
 
 
 @partial(jit, static_argnums=(0, 2))
@@ -45,28 +47,7 @@ def energy(net_apply, net_params, lpsi, state):
         logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
         return np.exp(logpsi_fliped - logpsi)
 
-    logpsi = lpsi(net_apply, net_params, state)
-    logpsi = logpsi[0] + logpsi[1] * 1j
-    E = 0
-    for i in range(state.shape[1] - 1):
-        E -= state[:, i] * state[:, i + 1] - amplitude_diff(state, i)
-    E -= amplitude_diff(state, -1)
-    return E
-
-
-@partial(jit, static_argnums=(0, 2))
-def energy_fori(net_apply, net_params, lpsi, state):
     @jit
-    def amplitude_diff(state, i):
-        """logpsi returns the real and the imaginary part seperately,
-        we therefor need to recombine them into a complex valued array"""
-        flip_i = np.ones(state.shape)
-        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
-        fliped = state * flip_i
-        logpsi_fliped = lpsi(net_apply, net_params, fliped)
-        logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
-        return np.exp(logpsi_fliped - logpsi)
-
     def body_fun(i, loop_carry):
         a, b = loop_carry
         E = a - b[:, i] * b[:, i + 1] - amplitude_diff(state, i)
@@ -80,6 +61,7 @@ def energy_fori(net_apply, net_params, lpsi, state):
     start_val = start_val[..., None]
     start_val = start_val.astype("complex64")
     E, _ = fori_loop(0, r, body_fun, (start_val, state))
+    E -= amplitude_diff(state, -1)
     return E
 
 
@@ -111,41 +93,28 @@ def magnetization(state):
 
 
 def callback(params, i, ax):
-    E, mag, end_time, start_time = params
-    print("iteration {} took {:.4f} secs.".format(i, end_time - start_time))
-    plt.cla()
-    ax.plot(E, label="Energy")
-    ax.plot(mag, label="Magnetization")
-    plt.legend()
-    plt.draw()
-    plt.pause(1.0 / 60.0)
+    E, mag, Time, epochs, gs_energy = params
+    epoch_mod = 100
+    if i > 0 and i % epoch_mod == 0 or i == epochs - 1:
+        print(
+            "{} epochs took {:.4f} seconds.".format(
+                epoch_mod, Time[i] - Time[i - epoch_mod]
+            )
+        )
+        plt.cla()
+        plt.axhline(gs_energy, label="Exact Energy", color="r")
+        ax.plot(E, label="Energy")
+        ax.plot(mag, label="Magnetization")
+        plt.legend()
+        plt.draw()
+        plt.pause(1.0 / 60.0)
 
 
-# @partial(jit, static_argnums=(0,))
-def step(i, net_apply, opt_update, get_params, opt_state, data):
+@partial(jit, static_argnums=(1, 2, 3))
+def step(i, net_apply, opt_update, get_params, opt_state, data, key):
     params = get_params(opt_state)
-    s = sample(net_apply, params, i, data)
+    key, s = sample(net_apply, params, i, data, key)
     e = energy(net_apply, params, lpsi, s)
     g = grad(net_apply, params, lpsi, s, e)
     var = energy_var(e)
-    return opt_update(i, g, opt_state), e.real.mean(), magnetization(s), var
-
-
-# @partial(jit, static_argnums=(0,))
-def step_fori(i, net_apply, opt_update, get_params, opt_state, data, key):
-    params = get_params(opt_state)
-    key, s = sample_fori(net_apply, params, i, data, key)
-    e = energy_fori(net_apply, params, lpsi, s)
-    g = grad(net_apply, params, lpsi, s, e)
-    var = energy_var(e)
-    return opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var
-
-
-# @partial(jit, static_argnums=(0,))
-def step2(i, net_apply, opt_update, get_params, opt_state, data):
-    params = get_params(opt_state)
-    s = sample(net_apply, params, i, data)
-    e = energy_fori(net_apply, params, lpsi, s)
-    g = grad(net_apply, params, lpsi, s, e)
-    var = energy_var(e)
-    return opt_update(i, g, opt_state), e.real.mean(), magnetization(s), var
+    return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var)
