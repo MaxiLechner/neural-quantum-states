e548c0d6c4c0c5ba1cabdfd941ad6578e408ccb3
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-11-19 00:19:33 +0100

    refactored sample func to make use of a fori_loop for speeding up jit compilation

--- sampler.py
+++ sampler.py
@@ -23,20 +23,7 @@ def compute_probs(arr):
 
 
 @partial(jit, static_argnums=(0,))
-def sample(net_apply, net_params, i, data):
-    key = random.PRNGKey(i + 1)
-    for i in range(data.shape[1]):
-        vi = net_apply(net_params, data)
-        probs = compute_probs(vi)
-        key, subkey = random.split(key)
-        sample = random.bernoulli(subkey, probs[:, i, 1]) * 2 - 1.0
-        sample = sample.reshape(data.shape[0], 1)
-        data = jax.ops.index_update(data, jax.ops.index[:, i], sample)
-    return data
-
-
-@partial(jit, static_argnums=(0,))
-def sample_fori(net_apply, net_params, i, data, key):
+def sample(net_apply, net_params, i, data, key):
     def body(i, loop_carry):
         key, data = loop_carry
         vi = net_apply(net_params, data)
