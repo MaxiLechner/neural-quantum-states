e3e4236e7f254acc29d853d7bacf002ef11ef3e8
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-11-20 15:23:39 +0100

    Added FLAGS. Now saves simulation results.

--- example_Ising1d.py
+++ example_Ising1d.py
@@ -1,32 +1,63 @@
-from ising1d import initialize_ising1d, step, callback
+from ising1d import step
+from network import small_resnet_1d
+
 import jax.numpy as np
+from jax import jit, random
+from jax.experimental import optimizers
 
 from time import time
-import matplotlib.pyplot as plt
+from pathlib import Path
+import warnings
+from absl import app, flags
 
-from absl import app
+
+FLAGS = flags.FLAGS
+flags.DEFINE_bool("Complex_warning", True, "Surpress Complex warning")
+flags.DEFINE_float("learning_rate", 1e-02, "Learning rate for training")
+flags.DEFINE_integer("batch_size", 100, "Batch size")
+flags.DEFINE_integer("num_spins", 10, "Number of spins")
+flags.DEFINE_integer("epochs", 200, "Number of epochs")
+flags.DEFINE_integer("seed", 0, "Seed for jax PRNG")
+flags.DEFINE_integer("width", 12, "Width of the model")
+flags.DEFINE_integer("filter_size", 3, "Size of the convolution filters")
+flags.DEFINE_string(
+    "filedir", "notebooks/results/", "Directory where data is saved.", short_name="f"
+)
+# flags.mark_flag_as_required('filename')
 
 
 def main(unused_argv):
-    batchSize = 100
-    numSpins = 10
-    lr = 1e-02
-    epochs = 300
-    net_apply, net_params, data, key, opt_init, opt_update, get_params = initialize_ising1d(
-        batchSize, numSpins, lr
-    )
+    if FLAGS.Complex_warning:
+        warnings.filterwarnings(
+            "ignore",
+            message="Casting complex values to real discards the imaginary part",
+        )
+    model = small_resnet_1d(FLAGS.width, FLAGS.filter_size)
+    net_init, net_apply = model
+    key = random.PRNGKey(FLAGS.seed)
+    key, subkey = random.split(key)
+    in_shape = (-1, FLAGS.num_spins, 1)
+    _, net_params = net_init(subkey, in_shape)
+    net_apply = jit(net_apply)
+    opt_init, opt_update, get_params = optimizers.adam(FLAGS.learning_rate)
+    data = np.zeros((FLAGS.batch_size, FLAGS.num_spins, 1), dtype=np.float32)
 
-    gs_energy = 1 - 1 / (np.sin(np.pi / (2 * (2 * numSpins + 1))))
+    gs_energy = 1 - 1 / (np.sin(np.pi / (2 * (2 * FLAGS.num_spins + 1))))
     E = []
     mag = []
     E_var = []
     Time = [time()]
-    fig, ax = plt.subplots()
-    plt.ion()
-    plt.show(block=False)
+    # _, ax = plt.subplots()
+    # plt.ion()
+    # plt.show(block=False)
 
     opt_state = opt_init(net_params)
-    for i in range(epochs):
+    print_every = 10
+    old_time = time()
+    print("Step\tEnergy\tMagnetization\tVariance\ttime/step")
+    print("---------------------------------------------------------")
+
+    for i in range(FLAGS.epochs):
         opt_state, key, energy, magnetization, var = step(
             i, net_apply, opt_update, get_params, opt_state, data, key
         )
@@ -34,11 +65,42 @@ def main(unused_argv):
         mag.append(magnetization)
         E_var.append(var.real)
         Time.append(time())
-        callback((E, mag, Time, epochs, gs_energy), i, ax)
+        # callback((E, mag, Time, FLAGS.epochs, gs_energy), i, ax)
+        if i % print_every == 0 and i > 0:
+            new_time = time()
+            print(
+                "{}\t{:.2f}\t{:.2f}\t{:.3f}\t{:.2f}".format(
+                    i, energy, magnetization, var.real, new_time - old_time
+                )
+            )
+            old_time = new_time
 
-    ax.plot(E_var, label="Variance")
-    plt.legend()
-    plt.show(block=True)
+    # ax.plot(E_var, label="Variance")
+    # plt.legend()
+    # plt.show(block=True)
+
+    directory = Path(FLAGS.filedir)
+    subdir = "ising1d_size_{}_bsize_{}_resnet1d_lr_{:.2f}_epochs_{}_width_{}_fs_{}/".format(
+        FLAGS.num_spins,
+        FLAGS.batch_size,
+        FLAGS.learning_rate,
+        FLAGS.epochs,
+        FLAGS.width,
+        FLAGS.filter_size,
+    )
+    directory = directory / subdir
+
+    if directory.is_dir():
+        np.save(directory / "exact_energy", gs_energy)
+        np.save(directory / "energy", E)
+        np.save(directory / "magnetization", mag)
+        np.save(directory / "energy_var", E_var)
+    else:
+        directory.mkdir(parents=True)
+        np.save(directory / "exact_energy", gs_energy)
+        np.save(directory / "energy", E)
+        np.save(directory / "magnetization", mag)
+        np.save(directory / "energy_var", E_var)
 
 
 if __name__ == "__main__":
