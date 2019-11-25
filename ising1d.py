daf5df2effd8d2f07cfe0551b0e1dd2ea04a82f0
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-11-25 16:43:55 +0100

    added heisenberg energy and step func plus some small changes

--- ising1d.py
+++ ising1d.py
@@ -17,6 +17,8 @@ from util import make_complex, apply_elementwise
 import matplotlib.pyplot as plt
 from functools import partial
 
+import pdb
+
 
 def initialize_ising1d(batchSize, numSpins, lr):
     M = (
@@ -39,7 +41,8 @@ def initialize_ising1d(batchSize, numSpins, lr):
 def energy(net_apply, net_params, lpsi, state):
     @jit
     def amplitude_diff(state, i):
-        """logpsi returns the real and the imaginary part seperately,
+        """Compute apmplitude ratio of logpsi and logpsi_fliped, where i has its
+        sign fliped. As logpsi returns the real and the imaginary part seperately,
         we therefor need to recombine them into a complex valued array"""
         flip_i = np.ones(state.shape)
         flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
@@ -51,18 +54,58 @@ def energy(net_apply, net_params, lpsi, state):
     @jit
     def body_fun(i, loop_carry):
         a, b = loop_carry
-        E = a - b[:, i] * b[:, i + 1] - amplitude_diff(state, i)
+        E = a - b[:, i] * b[:, i + 1] - amplitude_diff(b, i)
         return E, b
 
     logpsi = lpsi(net_apply, net_params, state)
     logpsi = logpsi[0] + logpsi[1] * 1j
 
-    r = state.shape[1] - 1
+    loop_start = 0
+    loop_end = state.shape[1] - 1
     start_val = np.zeros(state.shape[0])
     start_val = start_val[..., None]
     start_val = start_val.astype("complex64")
-    E, _ = fori_loop(0, r, body_fun, (start_val, state))
+    E, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, state))
     E -= amplitude_diff(state, -1)
+    return E
+
+
+@partial(jit, static_argnums=(0, 2))
+def energy_heisenberg(net_apply, net_params, lpsi, state):
+    # @jit
+    def amplitude_diff(state, i):
+        """compute apmplitude ratio of logpsi and logpsi_fliped, where i and i+1
+        have their sign fliped. As logpsi returns the real and the imaginary part
+        seperately, we therefor need to recombine them into a complex valued array"""
+        flip_i = np.ones(state.shape)
+        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
+        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i + 1], -1)
+        fliped = state * flip_i
+        logpsi_fliped = lpsi(net_apply, net_params, fliped)
+        logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
+        return np.exp(logpsi_fliped - logpsi)
+
+    # @jit
+    # def body_fun(i, loop_carry):
+    #     a, cond, b = loop_carry
+    #     E = a - b[:, i] * b[:, i + 1]  # - cond[:, i] * amplitude_diff(b, i)
+    #     return E, cond, b
+    @jit
+    def body_fun(i, loop_carry):
+        a, b = loop_carry
+        E = a - b[:, i] * b[:, i + 1]  # - amplitude_diff(b, i)
+        return E, b
+
+    # cond = (state * np.roll(state, -1, axis=1) * -1) + 1
+    logpsi = lpsi(net_apply, net_params, state)
+    logpsi = logpsi[0] + logpsi[1] * 1j
+
+    loop_start = 0
+    loop_end = state.shape[1] - 1
+    start_val = np.zeros(state.shape[0])
+    start_val = start_val[..., None]
+    start_val = start_val.astype("complex64")
+    E, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, state))
     return E
 
 
@@ -116,6 +159,16 @@ def step(i, net_apply, opt_update, get_params, opt_state, data, key):
     params = get_params(opt_state)
     key, s = sample(net_apply, params, i, data, key)
     e = energy(net_apply, params, lpsi, s)
+    g = grad(net_apply, params, lpsi, s, e)
+    var = energy_var(e)
+    return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var)
+
+
+# @partial(jit, static_argnums=(1, 2, 3))
+def step_heisenberg(i, net_apply, opt_update, get_params, opt_state, data, key):
+    params = get_params(opt_state)
+    key, s = sample(net_apply, params, i, data, key)
+    e = energy_heisenberg(net_apply, params, lpsi, s)
     g = grad(net_apply, params, lpsi, s, e)
     var = energy_var(e)
     return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var)
