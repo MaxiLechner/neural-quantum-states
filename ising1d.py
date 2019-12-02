f57a5e1cf483a180d5c2cd06d3981b9ead1f6c9b
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-12-02 16:40:38 +0100

    Refactored code in order to pass less variables around.

--- ising1d.py
+++ ising1d.py
@@ -9,9 +9,9 @@ from jax import jit
 from jax.lax import fori_loop
 from jax.experimental import optimizers
 
-from network import small_resnet_1d
-from wavefunction import lpsi
-from sampler import check_sample, sample
+from network import small_net_1d
+from wavefunction import log_amplitude_init
+from sampler import sample_init
 from util import make_complex, apply_elementwise
 
 import matplotlib.pyplot as plt
@@ -20,123 +20,112 @@ from functools import partial
 import pdb
 
 
-def initialize_ising1d(batchSize, numSpins, lr):
-    M = (
-        2 * 2
-    )  # number of possible values each input can take times 2 as lax.conv only works with real valued weights
-    FilterSize = 3
-    model = small_resnet_1d(M, FilterSize)
+def initialize_heisenberg_1d(
+    width, filter_size, seed, num_spins, lr, J, batch_size, pbc
+):
+    model = small_net_1d(width, filter_size)
     net_init, net_apply = model
-    key = random.PRNGKey(0)
+    key = random.PRNGKey(seed)
     key, subkey = random.split(key)
-    in_shape = (-1, numSpins, 1)
+    in_shape = (-1, num_spins, 1)
     _, net_params = net_init(subkey, in_shape)
     net_apply = jit(net_apply)
-    opt_init, opt_update, get_params = optimizers.adam(lr)
-    data = np.zeros((batchSize, numSpins, 1), dtype=np.float32)
-    return net_apply, net_params, data, key, opt_init, opt_update, get_params
+    sample = sample_init(net_apply)
+    logpsi = log_amplitude_init(net_apply)
+    energy = energy_heisenberg_init(logpsi, J, pbc)
+    grad = grad_init(logpsi)
+    opt_init, opt_update, get_params = optimizers.adam(
+        # optimizers.polynomial_decay(lr, 10, 0.00001, 3)
+        lr
+    )
+    opt_state = opt_init(net_params)
+    data = np.zeros((batch_size, num_spins, 1), dtype=np.float32)
+    step = step_init(energy, sample, grad, logpsi, data, opt_update, get_params)
+    return step, opt_state, key
 
 
-@partial(jit, static_argnums=(0, 2))
-def energy(net_apply, net_params, lpsi, state):
+def energy_ising_init(log_amplitude, pbc):
     @jit
-    def amplitude_diff(state, i):
-        """Compute amplitude ratio of logpsi and logpsi_fliped, where spin i has its
-        sign fliped. As logpsi returns the real and the imaginary part seperately,
-        we therefor need to recombine them into a complex valued array"""
-        flip_i = np.ones(state.shape)
-        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
-        fliped = state * flip_i
-        logpsi_fliped = lpsi(net_apply, net_params, fliped)
-        logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
-        return np.exp(logpsi_fliped - logpsi)
+    def energy(net_params, state):
+        @jit
+        def amplitude_diff(state, i):
+            """Compute amplitude ratio of logpsi and logpsi_fliped, where spin i has its
+            sign fliped. As logpsi returns the real and the imaginary part seperately,
+            we therefor need to recombine them into a complex valued array"""
+            flip_i = np.ones(state.shape)
+            flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
+            fliped = state * flip_i
+            logpsi_fliped = log_amplitude(net_params, fliped)
+            logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
+            return np.exp(logpsi_fliped - logpsi)
 
+        @jit
+        def body_fun(i, loop_carry):
+            E, s = loop_carry
+            E -= s[:, i] * s[:, i + 1] - amplitude_diff(s, i)
+            return E, s
+
+        logpsi = log_amplitude(net_params, state)
+        logpsi = logpsi[0] + logpsi[1] * 1j
+
+        loop_start = 0
+        loop_end = state.shape[1] - 1
+        start_val = np.zeros(state.shape[0])
+        start_val = start_val[..., None]
+        start_val = start_val.astype("complex64")
+        E, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, state))
+        E -= amplitude_diff(state, -1)
+        return E
+
+
+def energy_heisenberg_init(log_amplitude, J, pbc):
     @jit
-    def body_fun(i, loop_carry):
-        a, b = loop_carry
-        E = a - b[:, i] * b[:, i + 1] - amplitude_diff(b, i)
-        return E, b
+    def energy_heisenberg(net_params, state):
+        @jit
+        def amplitude_diff(state, i, j):
+            """compute apmplitude ratio of logpsi and logpsi_fliped, where i and i+1
+            have their sign fliped. As logpsi returns the real and the imaginary part
+            seperately, we therefor need to recombine them into a complex valued array"""
+            flip_i = np.ones(state.shape)
+            flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
+            flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, j], -1)
+            fliped = state * flip_i
+            logpsi_fliped = log_amplitude(net_params, fliped)
+            logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
+            return np.exp(logpsi_fliped - logpsi)
 
-    logpsi = lpsi(net_apply, net_params, state)
-    logpsi = logpsi[0] + logpsi[1] * 1j
+        @jit
+        def body_fun(i, loop_carry):
+            E, m, s = loop_carry
+            E += (
+                J
+                * 0.25
+                * (s[:, i] * s[:, i + 1] + m[:, i] * amplitude_diff(s, i, i + 1))
+            )
+            return E, m, s
 
-    loop_start = 0
-    loop_end = state.shape[1] - 1
-    start_val = np.zeros(state.shape[0])
-    start_val = start_val[..., None]
-    start_val = start_val.astype("complex64")
-    E, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, state))
-    E -= amplitude_diff(state, -1)
-    return E
+        def pbc_contrib(E):
+            E += J * 0.25 * state[:, -1] * state[:, 0]
+            E += J * 0.25 * mask[:, -1] * amplitude_diff(state, -1, 0)
+            return E
 
+        # sx*sx + sy*sy gives a contribution iff x[i]!=x[i+1]
+        mask = -state * np.roll(state, -1, axis=1) + 1
+        logpsi = log_amplitude(net_params, state)
+        logpsi = logpsi[0] + logpsi[1] * 1j
 
-# # @partial(jit, static_argnums=(0, 2))
-# def energy(net_apply, net_params, lpsi, state):
-#     # @jit
-#     def amplitude_diff(state, i):
-#         """Compute apmplitude ratio of logpsi and logpsi_fliped, where i has its
-#         sign fliped. As logpsi returns the real and the imaginary part seperately,
-#         we therefor need to recombine them into a complex valued array"""
-#         flip_i = np.ones(state.shape)
-#         flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
-#         fliped = state * flip_i
-#         logpsi_fliped = lpsi(net_apply, net_params, fliped)
-#         logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
-#         return np.exp(logpsi_fliped - logpsi)
+        loop_start = 0
+        loop_end = state.shape[1] - 1
+        start_val = np.zeros(state.shape[0])
+        start_val = start_val[..., None]
+        start_val = start_val.astype("complex64")
 
-#     logpsi = lpsi(net_apply, net_params, state)
-#     logpsi = logpsi[0] + logpsi[1] * 1j
+        E, _, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, mask, state))
+        # Can't use if statements in jitted code, need to use lax primitive instead.
+        E = jax.lax.cond(pbc, E, pbc_contrib, E, lambda E: np.add(E, 0))
+        return E
 
-#     loop_end = state.shape[1] - 1
-#     start_val = np.zeros(state.shape[0])
-#     start_val = start_val[..., None]
-#     start_val = start_val.astype("complex64")
-
-#     for i in range(loop_end):
-#         start_val -= state[:, i] * state[:, i + 1] + amplitude_diff(state, i)
-#         print(amplitude_diff(state, i))
-
-#     start_val -= amplitude_diff(state, -1)
-#     return start_val
-
-
-@partial(jit, static_argnums=(0, 2))
-def energy_heisenberg(net_apply, net_params, lpsi, state):
-    @jit
-    def amplitude_diff(state, i, j):
-        """compute apmplitude ratio of logpsi and logpsi_fliped, where i and i+1
-        have their sign fliped. As logpsi returns the real and the imaginary part
-        seperately, we therefor need to recombine them into a complex valued array"""
-        flip_i = np.ones(state.shape)
-        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
-        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, j], -1)
-        fliped = state * flip_i
-        logpsi_fliped = lpsi(net_apply, net_params, fliped)
-        logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
-        return np.exp(logpsi_fliped - logpsi)
-
-    @jit
-    def body_fun(i, loop_carry):
-        E, mask, state = loop_carry
-        E -= 0.25 * state[:, i] * state[:, i + 1] + 0.25 * mask[:, i] * amplitude_diff(
-            state, i, i + 1
-        )
-        return E, mask, state
-
-    mask = -state * np.roll(state, -1, axis=1) + 1
-    logpsi = lpsi(net_apply, net_params, state)
-    logpsi = logpsi[0] + logpsi[1] * 1j
-
-    loop_start = 0
-    loop_end = state.shape[1] - 1
-    start_val = np.zeros(state.shape[0])
-    start_val = start_val[..., None]
-    start_val = start_val.astype("complex64")
-
-    E, _, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, mask, state))
-    E -= 0.25 * state[:, -1] * state[:, 0]
-    E -= 0.25 * mask[:, -1] * amplitude_diff(state, -1, 0)
-    return E
+    return energy_heisenberg
 
 
 @jit
@@ -144,18 +133,21 @@ def energy_var(energy):
     return np.var(energy)
 
 
-@partial(jit, static_argnums=(0, 2))
-def grad(net_apply, net_params, lpsi, state, energy):
-    """computes the gradient (jacobian as lpsi returns two real numbers instead of one complex number)
-    of the local energy lpsi by computing jac and multipliying it component wise with the local energy eloc"""
-    eloc = energy.conj()
-    eloc_mean = np.mean(eloc)
-    eloc = eloc - eloc_mean
-    jac = jax.jacrev(lpsi, argnums=1)
-    jac = jac(net_apply, net_params, state)
-    jac = make_complex(jac)
-    jac = apply_elementwise(eloc, jac)
-    return jac
+def grad_init(log_amplitude):
+    @jit
+    def grad(net_params, state, energy):
+        """computes the gradient (jacobian as log_amplitude returns two real numbers instead of one complex number)
+        of the local energy log_amplitude by computing jac and multipliying it component wise with the local energy eloc"""
+        eloc = energy.conj()
+        eloc_mean = np.mean(eloc)
+        eloc = eloc - eloc_mean
+        jac = jax.jacrev(log_amplitude)
+        jac = jac(net_params, state)
+        jac = make_complex(jac)
+        jac = apply_elementwise(eloc, jac)
+        return jac
+
+    return grad
 
 
 @jit
@@ -181,33 +173,32 @@ def callback(params, i, ax):
         plt.draw()
         plt.pause(1.0 / 60.0)
 
-
-@partial(jit, static_argnums=(1, 2, 3))
-def step(i, net_apply, opt_update, get_params, opt_state, data, key):
-    params = get_params(opt_state)
-    key, s = sample(net_apply, params, data, key)
-    e = energy(net_apply, params, lpsi, s)
-    g = grad(net_apply, params, lpsi, s, e)
-    var = energy_var(e)
-    return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var, s)
+    # visualize histogram of weights
+    # pars = get_params(opt_state)
+    # plt.cla()
+    # _, ax = plt.subplots(1, 5, figsize=(30, 3))
+    # for i in range(len(ax)):
+    #     ax[i].hist(pars[i * 2][0].flatten())
+    #     # ax[i].set_title("layer", i * 2)
 
 
-@partial(jit, static_argnums=(1, 2, 3))
-def step_heisenberg(i, net_apply, opt_update, get_params, opt_state, data, key):
-    params = get_params(opt_state)
-    key, sampl = sample(net_apply, params, data, key)
-    # print(sampl.mean(), sampl.std())
-    # pdb.set_trace()
-    energy = energy_heisenberg(net_apply, params, lpsi, sampl)
-    # print(logpsi.mean(), logpsi.std())
-    # print(mask.sum() / 200.0, mask.mean(), mask.std())
-    g = grad(net_apply, params, lpsi, sampl, energy)
-    var = energy_var(energy)
-    return (
-        opt_update(i, g, opt_state),
-        key,
-        energy.real.mean(),
-        energy.imag.mean(),
-        magnetization(sampl),
-        var,
-    )
+def step_init(
+    energy_func, sample_func, grad_func, log_amplitude, data, opt_update, get_params
+):
+    @jit
+    def step(i, opt_state, key):
+        params = get_params(opt_state)
+        key, sample = sample_func(params, data, key)
+        energy = energy_func(params, sample)
+        g = grad_func(params, sample, energy)
+        var = energy_var(energy)
+        return (
+            opt_update(i, g, opt_state),
+            key,
+            energy.real.mean(),
+            energy.imag.mean(),
+            magnetization(sample),
+            var,
+        )
+
+    return step
