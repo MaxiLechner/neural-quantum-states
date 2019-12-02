800349667a03abee0ad90b692b77e44eefb94608
Lechner.Maximilian <Lechner.Maximilian@physik.uni-muenchen.de>
2019-12-02 12:34:03 +0100

    removed unnecessary variable i in sample func. added check_sample func for debugging purposes

--- sampler.py
+++ sampler.py
@@ -23,7 +23,7 @@ def compute_probs(arr):
 
 
 @partial(jit, static_argnums=(0,))
-def sample(net_apply, net_params, i, data, key):
+def sample(net_apply, net_params, data, key):
     def body(i, loop_carry):
         key, data = loop_carry
         vi = net_apply(net_params, data)
@@ -38,22 +38,53 @@ def sample(net_apply, net_params, i, data, key):
     return key, data
 
 
+# # @partial(jit, static_argnums=(0,))
+# def check_sample(net_apply, net_params, data, key):
+#     def body(i, loop_carry):
+#         key, data, _ = loop_carry
+#         vi = net_apply(net_params, data)
+#         probs = compute_probs(vi)
+#         key, subkey = random.split(key)
+#         sample = random.bernoulli(subkey, probs[:, i, 1]) * 2 - 1.0
+#         sample = sample.reshape(data.shape[0], 1)
+#         data = jax.ops.index_update(data, jax.ops.index[:, i], sample)
+#         return key, data, probs
+
+#     # pdb.set_trace()
+#     stuff = np.ones((100, 10, 2))
+#     # assert np.allclose(np.sum(probs, axis=2), 1)
+#     print("=" * 100)
+#     # print(data[0])
+#     # print("-" * 100)
+#     key, data, probs = fori_loop(0, data.shape[1], body, (key, data, stuff))
+#     print(data[0])
+#     print("_" * 100)
+#     print(probs[0])
+#     assert np.allclose(np.sum(probs, axis=2), 1)
+#     return key, data
+
+
 # @partial(jit, static_argnums=(0,))
-def check_sample(net_apply, net_params, i, data, key):
-    def body(i, loop_carry):
-        key, data, stuff = loop_carry
+def check_sample(net_apply, net_params, data, key):
+    for i in range(data.shape[1]):
         vi = net_apply(net_params, data)
         probs = compute_probs(vi)
         key, subkey = random.split(key)
         sample = random.bernoulli(subkey, probs[:, i, 1]) * 2 - 1.0
+        print("{}\t{}\t{}".format(i, vi[0, i], probs[0, i]))
+        # print("{}\t{}".format(i, probs[:, i, 1]))
+        print("{}\t{}".format(i, sample))
         sample = sample.reshape(data.shape[0], 1)
         data = jax.ops.index_update(data, jax.ops.index[:, i], sample)
-        return key, data, probs
-
+    print("=" * 100)
     # pdb.set_trace()
-    stuff = np.ones((100, 10, 2))
     # assert np.allclose(np.sum(probs, axis=2), 1)
-    key, data, probs = fori_loop(0, data.shape[1], body, (key, data, stuff))
-    # print(probs.sum(axis=2))
-    assert np.allclose(np.sum(probs, axis=2), 1)
+    # print("=" * 100)
+    # print(data[0])
+    # print("-" * 100)
+    # key, data, probs = fori_loop(0, data.shape[1], body, (key, data, stuff))
+    # print(data[0])
+    # print("_" * 100)
+    # print(probs[0])
+    # assert np.allclose(np.sum(probs, axis=2), 1)
     return key, data
