fixed variance func. some small additional changes

ising1d.py
@@ -11,7 +11,7 @@ from jax.experimental import optimizers
 
 from network import small_resnet_1d
 from wavefunction import lpsi
-from sampler import sample
+from sampler import check_sample, sample
 from util import make_complex, apply_elementwise
 
 import matplotlib.pyplot as plt
@@ -41,7 +41,7 @@ def initialize_ising1d(batchSize, numSpins, lr):
 def energy(net_apply, net_params, lpsi, state):
     @jit
     def amplitude_diff(state, i):
-        """Compute apmplitude ratio of logpsi and logpsi_fliped, where i has its
+        """Compute amplitude ratio of logpsi and logpsi_fliped, where spin i has its
         sign fliped. As logpsi returns the real and the imaginary part seperately,
         we therefor need to recombine them into a complex valued array"""
         flip_i = np.ones(state.shape)
@@ -70,33 +70,60 @@ def energy(net_apply, net_params, lpsi, state):
     return E
 
 
+# # @partial(jit, static_argnums=(0, 2))
+# def energy(net_apply, net_params, lpsi, state):
+#     # @jit
+#     def amplitude_diff(state, i):
+#         """Compute apmplitude ratio of logpsi and logpsi_fliped, where i has its
+#         sign fliped. As logpsi returns the real and the imaginary part seperately,
+#         we therefor need to recombine them into a complex valued array"""
+#         flip_i = np.ones(state.shape)
+#         flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
+#         fliped = state * flip_i
+#         logpsi_fliped = lpsi(net_apply, net_params, fliped)
+#         logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
+#         return np.exp(logpsi_fliped - logpsi)
+
+#     logpsi = lpsi(net_apply, net_params, state)
+#     logpsi = logpsi[0] + logpsi[1] * 1j
+
+#     loop_end = state.shape[1] - 1
+#     start_val = np.zeros(state.shape[0])
+#     start_val = start_val[..., None]
+#     start_val = start_val.astype("complex64")
+
+#     for i in range(loop_end):
+#         start_val -= state[:, i] * state[:, i + 1] + amplitude_diff(state, i)
+#         print(amplitude_diff(state, i))
+
+#     start_val -= amplitude_diff(state, -1)
+#     return start_val
+
+
 @partial(jit, static_argnums=(0, 2))
 def energy_heisenberg(net_apply, net_params, lpsi, state):
-    # @jit
-    def amplitude_diff(state, i):
+    @jit
+    def amplitude_diff(state, i, j):
         """compute apmplitude ratio of logpsi and logpsi_fliped, where i and i+1
         have their sign fliped. As logpsi returns the real and the imaginary part
         seperately, we therefor need to recombine them into a complex valued array"""
         flip_i = np.ones(state.shape)
         flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i], -1)
-        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, i + 1], -1)
+        flip_i = jax.ops.index_update(flip_i, jax.ops.index[:, j], -1)
         fliped = state * flip_i
         logpsi_fliped = lpsi(net_apply, net_params, fliped)
         logpsi_fliped = logpsi_fliped[0] + logpsi_fliped[1] * 1j
         return np.exp(logpsi_fliped - logpsi)
 
-    # @jit
-    # def body_fun(i, loop_carry):
-    #     a, cond, b = loop_carry
-    #     E = a - b[:, i] * b[:, i + 1]  # - cond[:, i] * amplitude_diff(b, i)
-    #     return E, cond, b
     @jit
     def body_fun(i, loop_carry):
-        a, b = loop_carry
-        E = a - b[:, i] * b[:, i + 1]  # - amplitude_diff(b, i)
-        return E, b
+        E, mask, state = loop_carry
+        E -= 0.5 * state[:, i] * state[:, i + 1] + 0.5 * mask[:, i] * amplitude_diff(
+            state, i, i + 1
+        )
+        return E, mask, state
 
-    # cond = (state * np.roll(state, -1, axis=1) * -1) + 1
+    mask = -state * np.roll(state, -1, axis=1) + 1
     logpsi = lpsi(net_apply, net_params, state)
     logpsi = logpsi[0] + logpsi[1] * 1j
 
@@ -105,15 +132,16 @@ def energy_heisenberg(net_apply, net_params, lpsi, state):
     start_val = np.zeros(state.shape[0])
     start_val = start_val[..., None]
     start_val = start_val.astype("complex64")
-    E, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, state))
+
+    E, _, _ = fori_loop(loop_start, loop_end, body_fun, (start_val, mask, state))
+    E -= 0.5 * state[:, -1] * state[:, 0]
+    E -= 0.5 * mask[:, -1] * amplitude_diff(state, -1, 0)
     return E
 
 
 @jit
 def energy_var(energy):
-    emean = np.mean(energy)
-    var = np.square(energy - emean)
-    return np.mean(var)
+    return np.var(energy)
 
 
 @partial(jit, static_argnums=(0, 2))
@@ -157,18 +185,32 @@ def callback(params, i, ax):
 @partial(jit, static_argnums=(1, 2, 3))
 def step(i, net_apply, opt_update, get_params, opt_state, data, key):
     params = get_params(opt_state)
-    key, s = sample(net_apply, params, i, data, key)
+    key, s = sample(net_apply, params, data, key)
     e = energy(net_apply, params, lpsi, s)
     g = grad(net_apply, params, lpsi, s, e)
     var = energy_var(e)
-    return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var)
+    return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var, s)
 
 
-# @partial(jit, static_argnums=(1, 2, 3))
+@partial(jit, static_argnums=(1, 2, 3))
 def step_heisenberg(i, net_apply, opt_update, get_params, opt_state, data, key):
     params = get_params(opt_state)
-    key, s = sample(net_apply, params, i, data, key)
-    e = energy_heisenberg(net_apply, params, lpsi, s)
-    g = grad(net_apply, params, lpsi, s, e)
-    var = energy_var(e)
-    return (opt_update(i, g, opt_state), key, e.real.mean(), magnetization(s), var)
+    key, sampl = sample(net_apply, params, data, key)
+    # print(sampl.mean(), sampl.std())
+    # if i == 0:
+    # key, subkey = random.split(key)
+    # probs = 0.5 * np.ones((100, 10, 1))
+    # sampl = random.bernoulli(subkey, probs) * 2 - 1.0
+    energy = energy_heisenberg(net_apply, params, lpsi, sampl)
+    # print(logpsi.mean(), logpsi.std())
+    # print(mask.sum() / 200.0, mask.mean(), mask.std())
+    g = grad(net_apply, params, lpsi, sampl, energy)
+    var = energy_var(energy)
+    return (
+        opt_update(i, g, opt_state),
+        key,
+        energy.real.mean(),
+        energy.imag.mean(),
+        magnetization(sampl),
+        var,
+    )