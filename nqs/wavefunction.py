66943985edd6b839ce39e3f0ebf9c640a9786f14
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-01-31 20:28:01 +0100

    changed reshape(N) to flatten as the former somehow doesn't work anymore due to a "TypeError: 'numpy.int64' object is not iterable'".

--- nqs/wavefunction.py
+++ nqs/wavefunction.py
@@ -19,12 +19,12 @@ def log_amplitude_init(net_apply):
         nc = np.linalg.norm(tc, 2, axis=2, keepdims=True) ** 2
 
         idx = (data + 1) / 2
-        idx = idx.astype("int32")
+        idx = idx.astype(np.int32)
         B, N, _ = data.shape
         splits = np.split(arr, B)
         splits = [i.reshape(N, 2) for i in splits]
         isplits = np.split(idx, B)
-        isplits = [i.reshape(N) for i in isplits]
+        isplits = [i.flatten() for i in isplits]
         vi = np.stack([splits[j][np.arange(N), isplits[j]] for j in range(B)]).reshape(
             B, N, 1
         )
