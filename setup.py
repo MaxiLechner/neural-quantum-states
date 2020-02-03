3207aec8302663ac9fb289bf14317a016ba439e3
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-02-03 20:22:55 +0100

    added version and license to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ INSTALL_REQUIRES = ["absl-py", "numpy", "jax>=0.1.55", "jaxlib>=0.1.37"]
 
 setuptools.setup(
     name="nqs",
+    version="0.0.1",
+    license="Apache 2.0",
     install_requires=INSTALL_REQUIRES,
     packages=setuptools.find_packages(),
     description="Neural Quantum States implemented in JAX.",
