6208436272f26567836ffd5777f9af250d786b7f
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-02-14 11:34:34 +0100

    Removed __future__.

--- examples/example_1d.py
+++ examples/example_1d.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from nqs.hamiltonian import initialize_model_1d
 
 import jax.numpy as np
