6912ea48486ed10969fce1a9c091d9234af1dfa4
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-04-21 16:17:26 +0200

    Removed real_to_complex and renamed some stuff.

--- nqs/sampler.py
+++ nqs/sampler.py
@@ -3,33 +3,30 @@ import jax.numpy as np
 from jax import jit, random
 from jax.lax import fori_loop
 
-from .util import real_to_complex
-
 
 @jit
-def compute_probs(arr):
-    arr = real_to_complex(arr)
-    tc = np.exp(arr)
-    nc = np.linalg.norm(tc, 2, axis=2, keepdims=True)
-    tc = tc / nc
-    probs = np.square(np.abs(tc))
+def prob(x):
+    x = np.exp(x)
+    norm = np.linalg.norm(x, 2, axis=2, keepdims=True)
+    x = x / norm
+    probs = np.square(np.abs(x))
     return probs
 
 
 def sample_init(net_apply):
     @jit
-    def sample(net_params, data, key):
+    def sample(net_params, config, key):
         def body(i, loop_carry):
-            key, data = loop_carry
-            vi = net_apply(net_params, data)
-            probs = compute_probs(vi)
+            key, config = loop_carry
+            out = net_apply(net_params, config)
+            probs = prob(out)
             key, subkey = random.split(key)
             sample = random.bernoulli(subkey, probs[:, i, 1]) * 2 - 1.0
             sample = sample[..., np.newaxis]
-            data = jax.ops.index_update(data, jax.ops.index[:, i], sample)
-            return key, data
+            config = jax.ops.index_update(config, jax.ops.index[:, i], sample)
+            return key, config
 
-        key, data = fori_loop(0, data.shape[1], body, (key, data))
-        return key, data
+        key, config = fori_loop(0, config.shape[1], body, (key, config))
+        return key, config
 
     return sample
