0c8086a0ebad3571f0de7152092854cf5f380aba
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-04-21 16:16:03 +0200

    Moved real_to_complex to network.

--- nqs/util.py
+++
@@ -1,8 +0,0 @@
-import jax.numpy as np
-from jax import jit
-
-
-@jit
-def real_to_complex(x):
-    """Turn real valued input array into complex output array."""
-    return x[:, :, [0, 2]] * np.exp(x[:, :, [1, 3]] * 1j)  # shape (N,M,4) -> (N,M,2)
