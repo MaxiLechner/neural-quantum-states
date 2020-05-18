7c1d5167478e9ac4505c16bc4df8cfb8a6dd426e
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-05-18 17:29:10 +0200

    Added MaskedConv1d and miscellaneous functions to net from flax.linear.

--- nqs/net.py
+++ nqs/net.py
@@ -1,8 +1,21 @@
-import jax
 import jax.numpy as jnp
-import flax
-from flax import nn
-from flax.nn.linear import MaskedConv1d
+from jax import lax, random
+
+from flax import nn, jax_utils
+from flax.nn import base, initializers
+
+import numpy as onp
+
+default_kernel_init = initializers.lecun_normal()
+
+
+def _conv_dimension_numbers(input_shape):
+    """Computes the dimension numbers based on the input shape."""
+    ndim = len(input_shape)
+    lhs_spec = (0, ndim - 1) + tuple(range(1, ndim - 1))
+    rhs_spec = (ndim - 1, ndim - 2) + tuple(range(0, ndim - 2))
+    out_spec = lhs_spec
+    return lax.ConvDimensionNumbers(lhs_spec, rhs_spec, out_spec)
 
 
 def one_hot(x, num_classes=2, net_dtype=jnp.float32):
@@ -15,6 +28,73 @@ def real_to_complex(x):
     return x[:, :, [0, 2]] * jnp.exp(x[:, :, [1, 3]] * 1j)  # shape (N,M,4) -> (N,M,2)
 
 
+class MaskedConv1d(base.Module):
+    """Masked 1D Convolution Module based on flax.nn.linear.Conv ."""
+
+    def apply(
+        self,
+        inputs,
+        features,
+        kernel_size,
+        is_first_layer=False,
+        strides=None,
+        padding="SAME",
+        input_dilation=None,
+        kernel_dilation=None,
+        feature_group_count=1,
+        bias=True,
+        dtype=jnp.float32,
+        precision=None,
+        kernel_init=default_kernel_init,
+        bias_init=initializers.zeros,
+    ):
+        """Applies a convolution to the inputs.
+
+    """
+
+        inputs = jnp.asarray(inputs, dtype)
+
+        assert len(kernel_size) == 1, "kernel_shape must be one dimensional"
+        assert kernel_size[0] % 2 != 0, "kernel_shape must be odd"
+
+        mask = onp.ones(kernel_size[0])
+        if is_first_layer:
+            i = (kernel_size[0] - 1) // 2
+        else:
+            i = (kernel_size[0] + 1) // 2
+        mask[i:] = 0
+        mask = jnp.asarray(mask[:, onp.newaxis, onp.newaxis], dtype)
+
+        if strides is None:
+            strides = (1,) * (inputs.ndim - 2)
+
+        in_features = inputs.shape[-1]
+        assert in_features % feature_group_count == 0
+        kernel_shape = kernel_size + (in_features // feature_group_count, features)
+        kernel = self.param("kernel", kernel_shape, kernel_init)
+        kernel = jnp.asarray(kernel, dtype)
+        kernel = kernel * mask
+
+        dimension_numbers = _conv_dimension_numbers(inputs.shape)
+        y = lax.conv_general_dilated(
+            inputs,
+            kernel,
+            strides,
+            padding,
+            lhs_dilation=input_dilation,
+            rhs_dilation=kernel_dilation,
+            dimension_numbers=dimension_numbers,
+            feature_group_count=feature_group_count,
+            precision=precision,
+        )
+
+        if bias:
+            bias = self.param("bias", (features,), bias_init)
+            bias = jnp.asarray(bias, dtype)
+            y = y + bias
+        return y
+
+
 class LSTM(nn.Module):
     """LSTM encoder. Turns a sequence of vectors into a vector."""
 
@@ -22,7 +102,7 @@ class LSTM(nn.Module):
         # inputs.shape = <float32>[batch_size, seq_length, emb_size].
         batch_size = inputs.shape[0]
         carry = nn.LSTMCell.initialize_carry(key, (batch_size,), hidden_size)
-        _, outputs = flax.jax_utils.scan_in_dim(
+        _, outputs = jax_utils.scan_in_dim(
             nn.LSTMCell.partial(name="lstm_cell"), carry, inputs, axis=1
         )
         return outputs
@@ -30,18 +110,12 @@ class LSTM(nn.Module):
 
 @nn.module
 def lstm(x, key, depth, hidden_size=24, use_one_hot=True, dtype=jnp.float32):
-    keys = jax.random.split(key, depth)
+    keys = random.split(key, depth)
     if use_one_hot:
         x = one_hot(x)
     for i in range(depth):
         x = LSTM(x, keys[i], hidden_size=hidden_size)
         x = nn.relu(x)
-    # x = LSTM(x, keys[0], hidden_size=hidden_size)
-    # x = nn.relu(x)
-    # x = LSTM(x, keys[1], hidden_size=hidden_size)
-    # x = nn.relu(x)
-    # x = LSTM(x, keys[2], hidden_size=hidden_size)
-    # x = nn.relu(x)
     x = nn.Dense(x, 4)
     x = real_to_complex(x)
     return x
