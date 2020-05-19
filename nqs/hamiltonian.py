14c5c4ab6bd3e3673f816c8f4fa95a7072da067c
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-05-19 16:43:58 +0200

    Added J1J2 model and SzSz operator.

--- nqs/hamiltonian.py
+++ nqs/hamiltonian.py
@@ -1,5 +1,5 @@
 import jax
-from jax import jit, random, config
+from jax import vmap, jit, random, config
 import jax.numpy as np
 from jax.lax import fori_loop
 
@@ -21,6 +21,7 @@ def initialize_model_1d(
     num_spins,
     lr,
     J,
+    J2,
     batch_size,
     hidden_size,
     depth,
@@ -47,6 +48,7 @@ def initialize_model_1d(
     energy_dispatch = {
         "ising1d": energy_ising_1d_init,
         "heisenberg1d": energy_heisenberg_1d_init,
+        "J1J21d": energy_J1J2_1d_init,
     }
 
     key = random.PRNGKey(seed)
@@ -71,6 +73,10 @@ def initialize_model_1d(
 
     try:
         energy_init = energy_dispatch[hamiltonian]
+        if hamiltonian == "J1J21d":
+            energy_dict = {"J1": J, "J2": J2, "pbc": pbc, "c_dtype": c_dtype}
+        else:
+            energy_dict = {"J": J, "pbc": pbc, "c_dtype": c_dtype}
     except KeyError:
         print(
             f"{hamiltonian} is not a valid hamiltonian. You can choose between ising1d and heisenberg1d."
@@ -87,13 +93,13 @@ def initialize_model_1d(
 
     init_config = np.zeros((batch_size, num_spins, 1), dtype=f_dtype)
     sample_fn = sample_init(init_config)
-    energy_fn = energy_init(J, pbc, c_dtype)
+    energy_fn = energy_init(**energy_dict)
     optimizer = flax.optim.Adam(learning_rate=lr).create(model)
     step = step_init(energy_fn, sample_fn, energy_var, magnetization)
     return (step, key, energy_fn, sample_fn, init_config, optimizer)
 
 
-def energy_ising_1d_init(J, pbc, c_dtype):
+def energy_ising_1d_init(J=None, pbc=None, c_dtype=None):
     @jit
     def energy(model, config):
         @jit
@@ -130,7 +136,7 @@ def energy_ising_1d_init(J, pbc, c_dtype):
     return energy
 
 
-def energy_heisenberg_1d_init(J, pbc, c_dtype):
+def energy_heisenberg_1d_init(J=None, pbc=None, c_dtype=None):
     @jit
     def energy(model, config):
         @jit
@@ -171,6 +177,60 @@ def energy_heisenberg_1d_init(J, pbc, c_dtype):
     return energy
 
 
+def energy_J1J2_1d_init(J1=None, J2=None, pbc=None, c_dtype=None):
+    @jit
+    def energy(model, config):
+        @jit
+        def amplitude_diff(config, i, k):
+            """compute apmplitude ratio of logpsi and logpsi_flipped, where i and i+k
+            have their sign flipped. As logpsi returns the real and the imaginary part
+            seperately, we therefor need to recombine them into a complex valued array"""
+            flipped = jax.ops.index_mul(config, jax.ops.index[:, i], -1)
+            flipped = jax.ops.index_mul(flipped, jax.ops.index[:, (i + k) % N], -1)
+            logpsi_flipped = log_amplitude(model, flipped)
+            return np.exp(logpsi_flipped - logpsi)
+
+        @jit
+        def body_fun1(i, loop_carry):
+            E, m, s = loop_carry
+            E += (
+                J1
+                * 0.25
+                * (m[:, i] * amplitude_diff(s, i, 1) + s[:, i] * s[:, (i + 1) % N])
+            )
+            return E, m, s
+
+        @jit
+        def body_fun2(i, loop_carry):
+            E, m, s = loop_carry
+            E += (
+                J2
+                * 0.25
+                * (m[:, i] * amplitude_diff(s, i, 2) + s[:, i] * s[:, (i + 2) % N])
+            )
+            return E, m, s
+
+        # sx*sx + sy*sy gives a contribution iff x[i]!=x[i+1]
+        mask1 = config * np.roll(config, -1, axis=1) - 1
+        mask2 = config * np.roll(config, -2, axis=1) - 1
+        logpsi = log_amplitude(model, config)
+
+        start = 0
+        shape = config.shape
+        B, N, _ = shape
+        # Can't use if statements in jitted code, need to use lax primitive instead.
+        end1 = jax.lax.cond(pbc, N, lambda x: x, N - 1, lambda x: x)
+        end2 = jax.lax.cond(pbc, N, lambda x: x, N - 2, lambda x: x)
+
+        start_val = np.zeros(B, dtype=c_dtype)[..., None]
+
+        E1, _, _ = fori_loop(start, end1, body_fun1, (start_val, mask1, config))
+        E2, _, _ = fori_loop(start, end2, body_fun2, (start_val, mask2, config))
+        return E1 + E2
+
+    return energy
+
+
 @jit
 def energy_var(energy):
     return np.var(energy.real)
@@ -180,6 +240,16 @@ def energy_var(energy):
 def magnetization(config):
     mag = np.sum(config, axis=1)
     return np.mean(mag)
+
+
+@jit
+def SzSz(config, i, j):
+    def mul(i, j):
+        return np.mean(config[:, i] * config[:, j])
+
+    mul = vmap(mul, in_axes=(0, None))
+    mul = vmap(mul, in_axes=(None, 0))
+    return mul(np.array(i), np.array(j))
 
 
 def callback(params, i, ax):
