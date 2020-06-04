f2f94b9ad84387e7ff0b46ffa645b0eedcef3d78
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-06-04 12:42:39 +0200

    Added vising_energy function to check if using vmap over explicit loops is faster. Plus some smaller changes to do moving sampling code into the nn.

--- nqs/hamiltonian.py
+++ nqs/hamiltonian.py
@@ -5,12 +5,12 @@ from jax.lax import fori_loop
 
 import flax
 
-from .net import conv, lstm
-from .sampler import sample_init
+from .networks import conv, lstm
 from .optim import step_init
 from .wavefunction import log_amplitude
 
 import matplotlib.pyplot as plt
+from functools import partial
 
 
 def initialize_model_1d(
@@ -47,22 +47,32 @@ def initialize_model_1d(
 
     energy_dispatch = {
         "ising1d": energy_ising_1d_init,
+        "vising1d": energy_vmap_ising_1d_init,
         "heisenberg1d": energy_heisenberg_1d_init,
         "J1J21d": energy_J1J2_1d_init,
     }
 
     key = random.PRNGKey(seed)
-    key, subkey, carry_key = random.split(key, 3)
+    key, subkey, init_key = random.split(key, 3)
+    init_config = np.zeros((batch_size, num_spins, 1), dtype=f_dtype)
 
     try:
         net = net_dispatch[network]
         if network == "conv":
             module = net.partial(
-                depth=depth, features=width, kernel_size=filter_size, use_one_hot=True
+                depth=depth,
+                features=width,
+                kernel_size=filter_size,
+                use_one_hot=one_hot,
+                init_config=init_config,
             )
         elif network == "lstm":
             module = net.partial(
-                hidden_size=hidden_size, key=carry_key, depth=depth, use_one_hot=one_hot
+                hidden_size=hidden_size,
+                init_key=init_key,
+                depth=depth,
+                use_one_hot=one_hot,
+                init_config=init_config,
             )
 
     except KeyError:
@@ -91,12 +101,10 @@ def initialize_model_1d(
     _, params = module.init_by_shape(subkey, [in_shape])
     model = flax.nn.Model(module, params)
 
-    init_config = np.zeros((batch_size, num_spins, 1), dtype=f_dtype)
-    sample_fn = sample_init(init_config)
     energy_fn = energy_init(**energy_dict)
     optimizer = flax.optim.Adam(learning_rate=lr).create(model)
-    step = step_init(energy_fn, sample_fn, energy_var, magnetization)
-    return (step, key, energy_fn, sample_fn, init_config, optimizer)
+    step = step_init(energy_fn, energy_var, magnetization)
+    return (step, key, energy_fn, init_config, optimizer)
 
 
 def energy_ising_1d_init(J=None, pbc=None, c_dtype=None):
@@ -119,16 +127,46 @@ def energy_ising_1d_init(J=None, pbc=None, c_dtype=None):
         logpsi = log_amplitude(model, config)
         logpsi = logpsi.astype(c_dtype)
 
-        start = 0
         B, N, _ = config.shape
         # Can't use if statements in jitted code, need to use lax primitive instead.
         end = jax.lax.cond(pbc, N, lambda x: x, N - 1, lambda x: x)
+        start = 0
         start_val = np.zeros(B, dtype=c_dtype)[..., None]
 
         E, _ = fori_loop(start, end, body_fun, (start_val, config))
         E = jax.lax.cond(
             pbc, E, lambda x: x, E, lambda x: x - amplitude_diff(config, -1)
         )
+        return E
+
+    return energy
+
+
+def energy_vmap_ising_1d_init(J=None, pbc=None, c_dtype=None):
+    @jit
+    def energy(model, config):
+        @jit
+        def amplitude_diff(config, i):
+            """Compute amplitude ratio of logpsi and logpsi_flipped, where spin i has its
+            sign flipped."""
+            flipped = jax.ops.index_mul(config, jax.ops.index[:, i], -1)
+            logpsi_flipped = log_amplitude(model, flipped)
+            return np.exp(logpsi_flipped - logpsi)
+
+        vmap_amplitude_diff = vmap(partial(amplitude_diff, config), out_axes=1)
+
+        logpsi = log_amplitude(model, config)
+        logpsi = logpsi.astype(c_dtype)
+
+        _, N, _ = config.shape
+        # Can't use if statements in jitted code, need to use lax primitive instead.
+        end = jax.lax.cond(pbc, N, lambda x: x, N - 1, lambda x: x)
+
+        idx = np.arange(N)
+        E0 = np.sum(config[:, :end] * np.roll(config, -1, axis=1)[:, :end], axis=1)
+        E1 = np.sum(vmap_amplitude_diff(idx), axis=1)
+
+        E = -(E0 + J * E1)
         return E
 
     return energy
