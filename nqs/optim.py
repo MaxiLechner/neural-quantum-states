eb741b9eaead24684bc2840b5245007f2bca3589
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-08-29 16:34:43 +0200

    Removed learning rate function, added lr to step func. Added init_config to step func for iterative retraining.

--- nqs/optim.py
+++ nqs/optim.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from jax import jit, grad
+from jax import jit, grad, tree_multimap
 
 from .wavefunction import log_amplitude
 
@@ -12,17 +12,17 @@ def loss(model, config, energy):
     return 2 * jnp.mean(jnp.real(energy * lpsi))
 
 
-def step_init(energy_fn, learning_rate_fn, energy_var, magnetization):
+def step_init(energy_fn, energy_var, magnetization):
     @jit
-    def step(optimizer, key):
+    def step(optimizer, key, lr, init_config):
         model = optimizer.target
-        key, config = model.sample(key=key)
+        key, config = model.sample(key=key, init_config=init_config)
         energy = energy_fn(model, config)
         grad_loss = grad(loss)(model, config, energy)
         var = energy_var(energy)
         mag = magnetization(config)
-        lr = learning_rate_fn(optimizer.state.step)
+        grad_loss = tree_multimap(lambda g: jnp.clip(g, -1, 1), grad_loss)
         opt_update = optimizer.apply_gradient(grad_loss, learning_rate=lr)
-        return opt_update, key, energy, mag, var, lr
+        return opt_update, key, energy, mag, var
 
     return step
