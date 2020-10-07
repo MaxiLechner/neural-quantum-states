c59f3737bb57a7cbeec3a6f6ceb7b02e785d1127
Lechner.Maximilian <maxi.lechner@hotmail.de>
2020-10-07 12:11:18 +0200

    Added flax to setup. Further change required version numbers.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-INSTALL_REQUIRES = ["absl-py", "numpy", "jax>=0.1.55", "jaxlib>=0.1.37"]
+INSTALL_REQUIRES = ["absl-py", "numpy", "jax>=0.2.1", "jaxlib>=0.1.55", "flax>=0.2.2"]
 
 setuptools.setup(
     name="nqs",
-    version="0.0.1",
+    version="0.0.2",
     license="Apache 2.0",
     install_requires=INSTALL_REQUIRES,
     packages=setuptools.find_packages(),
